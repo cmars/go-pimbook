8d0c4aae2141f5bbed29bb277b51590dbb412c60
Casey Marshall <git@cmars.tech>
2018-12-27 23:25:28 -0600

    Replace quicktest with testify/assert.
    Simpler and less transitive dependencies.

--- poly/poly_test.go
+++ poly/poly_test.go
@@ -4,122 +4,99 @@ import (
 	"math/big"
 	"testing"
 
-	qt "github.com/frankban/quicktest"
-	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/cmars/pimbook/poly"
 )
 
-var ratSliceEquals = qt.CmpEquals(cmp.Transformer("ratstring", func(in *big.Rat) string { return in.String() }))
+func TestAdd2PolyDegree2(t *testing.T) {
+	p1 := poly.NewInt(1, 2, 1)
+	p2 := poly.NewInt(3, 5, 7)
+	sum := poly.New().Add(p1, p2)
+	assert.Equal(t, sum, poly.NewInt(4, 7, 8))
+}
 
-func TestAdd(t *testing.T) {
-	t.Run("add 2 polys, both degree=2", func(t *testing.T) {
-		c := qt.New(t)
-		p1 := poly.NewInt(1, 2, 1)
-		p2 := poly.NewInt(3, 5, 7)
-		sum := poly.New().Add(p1, p2)
-		c.Assert(sum.Coeff(), ratSliceEquals, poly.NewInt(4, 7, 8).Coeff())
-	})
-	t.Run("add 2 polys, unequal degree, smaller first", func(t *testing.T) {
-		c := qt.New(t)
-		p1 := poly.NewInt(1, 2)
-		p2 := poly.NewInt(3, 5, 7)
-		sum := poly.New().Add(p1, p2)
-		c.Assert(sum.Coeff(), ratSliceEquals, []*big.Rat{
-			big.NewRat(4, 1), big.NewRat(7, 1), big.NewRat(7, 1)})
-	})
-	t.Run("add 2 polys, unequal degree, larger first", func(t *testing.T) {
-		c := qt.New(t)
-		p1 := poly.NewInt(1, 2, 1)
-		p2 := poly.NewInt(3, 5)
-		sum := poly.New().Add(p1, p2)
-		c.Assert(sum.Coeff(), ratSliceEquals, []*big.Rat{
-			big.NewRat(4, 1), big.NewRat(7, 1), big.NewRat(1, 1)})
-	})
-	t.Run("add 0 poly first", func(t *testing.T) {
-		c := qt.New(t)
-		p1 := poly.New()
-		p2 := poly.NewInt(3, 5, 7)
-		sum := poly.New().Add(p1, p2)
-		c.Assert(sum.Coeff(), ratSliceEquals, []*big.Rat{
-			big.NewRat(3, 1), big.NewRat(5, 1), big.NewRat(7, 1)})
-	})
-	t.Run("add 0 poly last", func(t *testing.T) {
-		c := qt.New(t)
-		p1 := poly.NewInt(3, 5, 7)
-		p2 := poly.New()
-		sum := poly.New().Add(p1, p2)
-		c.Assert(sum.Coeff(), ratSliceEquals, []*big.Rat{
-			big.NewRat(3, 1), big.NewRat(5, 1), big.NewRat(7, 1)})
-	})
-	t.Run("add to zero, self", func(t *testing.T) {
-		c := qt.New(t)
-		p1 := poly.New()
-		sum := p1.Add(p1, poly.NewInt(3, 5, 7))
-		c.Assert(sum.Coeff(), ratSliceEquals, []*big.Rat{
-			big.NewRat(3, 1), big.NewRat(5, 1), big.NewRat(7, 1)})
-	})
-	t.Run("add to nonzero, self", func(t *testing.T) {
-		c := qt.New(t)
-		p1 := poly.NewInt(1, 2, 1)
-		sum := p1.Add(p1, poly.NewInt(3, 5, 7))
-		c.Assert(sum.Coeff(), ratSliceEquals, poly.NewInt(4, 7, 8).Coeff())
-	})
+func TestAdd2PolyUnequalDegree(t *testing.T) {
+	p1 := poly.NewInt(1, 2)
+	p2 := poly.NewInt(3, 5, 7)
+	sum := poly.New().Add(p1, p2)
+	assert.Equal(t, sum, poly.NewInt(4, 7, 7))
+}
+
+func TestAdd2PolyUnequalDegreeLargerFirst(t *testing.T) {
+	p1 := poly.NewInt(1, 2, 1)
+	p2 := poly.NewInt(3, 5)
+	sum := poly.New().Add(p1, p2)
+	assert.Equal(t, sum, poly.NewInt(4, 7, 1))
+}
+
+func TestAddZeroPolyFirst(t *testing.T) {
+	p1 := poly.New()
+	p2 := poly.NewInt(3, 5, 7)
+	sum := poly.New().Add(p1, p2)
+	assert.Equal(t, sum, poly.NewInt(3, 5, 7))
+}
+
+func TestAddZeroPolyLast(t *testing.T) {
+	p1 := poly.NewInt(3, 5, 7)
+	p2 := poly.New()
+	sum := poly.New().Add(p1, p2)
+	assert.Equal(t, sum, poly.NewInt(3, 5, 7))
+}
+
+func TestAddToZeroSelf(t *testing.T) {
+	p1 := poly.New()
+	sum := p1.Add(p1, poly.NewInt(3, 5, 7))
+	assert.Equal(t, sum, poly.NewInt(3, 5, 7))
+}
+
+func TestAddToNonzeroSelf(t *testing.T) {
+	p1 := poly.NewInt(1, 2, 1)
+	sum := p1.Add(p1, poly.NewInt(3, 5, 7))
+	assert.Equal(t, sum, poly.NewInt(4, 7, 8))
 }
 
 func TestString(t *testing.T) {
-	t.Run("strings", func(t *testing.T) {
-		c := qt.New(t)
-		c.Assert(poly.NewInt(7).String(), qt.Equals, "7")
-		c.Assert(poly.NewInt(7, 2).String(), qt.Equals, "7+2x")
-		c.Assert(poly.NewInt(7, 2, 9).String(), qt.Equals, "7+2x+9x^2")
-		c.Assert(poly.NewInt(-7, -2, -9).String(), qt.Equals, "-7-2x-9x^2")
-		c.Assert(poly.NewInt(7, 0, 9).String(), qt.Equals, "7+9x^2")
-		c.Assert(poly.NewInt(0, 0, 9).String(), qt.Equals, "9x^2")
-	})
+	assert.Equal(t, poly.NewInt(7).String(), "7")
+	assert.Equal(t, poly.NewInt(7, 2).String(), "7+2x")
+	assert.Equal(t, poly.NewInt(7, 2, 9).String(), "7+2x+9x^2")
+	assert.Equal(t, poly.NewInt(-7, -2, -9).String(), "-7-2x-9x^2")
+	assert.Equal(t, poly.NewInt(7, 0, 9).String(), "7+9x^2")
+	assert.Equal(t, poly.NewInt(0, 0, 9).String(), "9x^2")
 }
 
 func TestMul(t *testing.T) {
-	t.Run("multiply 2 polys, both degree=2", func(t *testing.T) {
-		c := qt.New(t)
-		p1 := poly.NewInt(1, 2, 1)
-		p2 := poly.NewInt(7, 5, 3)
-		prod := poly.New().Mul(p1, p2)
-		c.Assert(prod.Coeff(), ratSliceEquals, poly.NewInt(7, 19, 20, 11, 3).Coeff())
-	})
+	p1 := poly.NewInt(1, 2, 1)
+	p2 := poly.NewInt(7, 5, 3)
+	prod := poly.New().Mul(p1, p2)
+	assert.Equal(t, prod, poly.NewInt(7, 19, 20, 11, 3))
 }
 
 func TestEval(t *testing.T) {
-	t.Run("evaluate poly at x", func(t *testing.T) {
-		c := qt.New(t)
-		p := poly.NewInt(1, 2, 1)
-		f9 := p.Eval(big.NewRat(9, 1))
-		c.Assert(f9.String(), qt.Equals, "100/1")
+	p := poly.NewInt(1, 2, 1)
+	f9 := p.Eval(big.NewRat(9, 1))
+	assert.Equal(t, f9.String(), "100/1")
 
-		p = poly.New()
-		f9 = p.Eval(big.NewRat(9, 1))
-		c.Assert(f9.String(), qt.Equals, "0/1")
+	p = poly.New()
+	f9 = p.Eval(big.NewRat(9, 1))
+	assert.Equal(t, f9.String(), "0/1")
 
-		p = poly.NewInt(0, 0, 0, 1)
-		f9 = p.Eval(big.NewRat(9, 1))
-		c.Assert(f9.String(), qt.Equals, "729/1")
-	})
+	p = poly.NewInt(0, 0, 0, 1)
+	f9 = p.Eval(big.NewRat(9, 1))
+	assert.Equal(t, f9.String(), "729/1")
 }
 
 func TestInterpolate(t *testing.T) {
-	t.Run("interpolate poly for given points", func(t *testing.T) {
-		c := qt.New(t)
-		p, err := poly.Interpolate(poly.NewPointInt(1, 1))
-		c.Assert(err, qt.IsNil)
-		c.Assert(p.Coeff(), ratSliceEquals, poly.NewInt(1).Coeff())
+	p, err := poly.Interpolate(poly.NewPointInt(1, 1))
+	assert.Nil(t, err)
+	assert.Equal(t, p, poly.NewInt(1))
 
-		p, err = poly.Interpolate(poly.NewPointInt(1, 1), poly.NewPointInt(2, 0))
-		c.Assert(err, qt.IsNil)
-		c.Assert(p.Coeff(), ratSliceEquals, poly.NewInt(2, -1).Coeff())
+	p, err = poly.Interpolate(poly.NewPointInt(1, 1), poly.NewPointInt(2, 0))
+	assert.Nil(t, err)
+	assert.Equal(t, p, poly.NewInt(2, -1))
 
-		p, err = poly.Interpolate(poly.NewPointInt(1, 1), poly.NewPointInt(2, 4), poly.NewPointInt(7, 9))
-		c.Assert(err, qt.IsNil)
-		c.Assert(p.Coeff(), ratSliceEquals, poly.New(
-			big.NewRat(-8, 3), big.NewRat(4, 1), big.NewRat(-1, 3)).Coeff())
-	})
+	p, err = poly.Interpolate(poly.NewPointInt(1, 1), poly.NewPointInt(2, 4), poly.NewPointInt(7, 9))
+	assert.Nil(t, err)
+	assert.Equal(t, p, poly.New(
+		big.NewRat(-8, 3), big.NewRat(4, 1), big.NewRat(-1, 3)))
 }
